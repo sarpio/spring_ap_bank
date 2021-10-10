8b3992b3b44a0120187ddd10698668e4b06a5bc6
sarpio <kontakt@piotrsarnecki.com>
2021-10-10 18:17:45 +0200

    Added mysql support

--- account-service/src/main/java/com/example/demo/rest/AccountController.java
+++ account-service/src/main/java/com/example/demo/rest/AccountController.java
@@ -1,7 +1,17 @@
 package com.example.demo.rest;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.rest.dto.AccountDTO;
+import com.example.demo.services.AccountServices;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequiredArgsConstructor
 public class AccountController {
+
 }
