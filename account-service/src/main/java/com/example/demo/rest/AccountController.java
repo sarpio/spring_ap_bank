659681b81a1f72a9ae004ba17ae9b0dc636f8232
sarpio <kontakt@piotrsarnecki.com>
2021-10-24 19:24:50 +0200

    Account with default Controller Configuration

--- account-service/src/main/java/com/example/demo/rest/AccountController.java
+++ account-service/src/main/java/com/example/demo/rest/AccountController.java
@@ -12,6 +12,7 @@ import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/account")
 public class AccountController {
 
     private final AccountServices accountServices;
--- account-service/src/main/java/com/example/demo/services/AccountServices.java
+++ account-service/src/main/java/com/example/demo/services/AccountServices.java
@@ -1,4 +1,26 @@
 package com.example.demo.services;
 
+import com.example.demo.rest.dto.AccountDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
 public class AccountServices {
+    public List<AccountDTO> findAllAccounts() {
+        return null;
+    }
+
+    public AccountDTO findAccountById(UUID id) {
+        return null;
+    }
+
+    public AccountDTO createAccount(AccountDTO dto) {
+        return null;
+    }
+
+    public String deleteAccountById(UUID id) {
+        return "Account removed";
+    }
 }
