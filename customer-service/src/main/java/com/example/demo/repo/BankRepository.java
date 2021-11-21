f8566c61a82ab4bc81138c0de44c74fa09c75dfb
sarpio <kontakt@piotrsarnecki.com>
2021-11-21 19:57:15 +0100

    Full Configured All Services: Account, Operation, Customer

--- customer-service/src/main/java/com/example/demo/repo/BankRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repo;
-
-import com.example.demo.entity.Bank;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface BankRepository extends JpaRepository<Bank, UUID> {
-}
