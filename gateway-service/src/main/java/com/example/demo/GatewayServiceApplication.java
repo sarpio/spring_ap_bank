697c34eba54aef569df05257102f35bac9a997b7
sarpio <kontakt@piotrsarnecki.com>
2021-11-22 17:39:04 +0100

    Full Configured All Services: Account, Operation, Customer

--- gateway-service/src/main/java/com/example/demo/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/demo/GatewayServiceApplication.java
@@ -2,9 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
