739e0e84e8ee9320cc816f6809e1e47b564e01e7
sarpio <kontakt@piotrsarnecki.com>
2021-11-23 08:59:21 +0100

    Full Configured All Services: Account, Operation, Customer

--- gateway-service/src/main/java/com/example/demo/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/demo/GatewayServiceApplication.java
@@ -2,12 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
-import java.util.Date;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -16,15 +12,4 @@ public class GatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
-
-//    @Bean
-//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-//        return builder.routes()
-//                .route(a -> a
-//                        .path("/api/account/**")
-//                        .filters(f -> f.rewritePath("/api/account/(?<segment>.*)", "/${segment}")
-//                                .addResponseHeader("X-Response-Time", new Date().toString()))
-//                        .uri("lb://ACCOUNT-SERVICE")).build();
-//    }
-
 }
