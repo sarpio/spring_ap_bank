a7564519d0af371e9fa6d488173c11541b8edae4
sarpio <kontakt@piotrsarnecki.com>
2022-02-02 17:27:09 +0100

    FrontEnd improvement

--- account-service/src/main/java/com/example/demo/AccountServiceApplication.java
+++ account-service/src/main/java/com/example/demo/AccountServiceApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@RefreshScope
+
 @EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
